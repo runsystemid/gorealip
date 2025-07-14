6172e0cf95eefc7319b3491832773a5c8aeb6731
Tommy <tommy.nurwantoro@gmail.com>
2025-07-14 14:34:46 +0700

    add golog implementation

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.4
 require (
 	github.com/gofiber/fiber/v2 v2.52.8
 	github.com/realclientip/realclientip-go v1.0.0
+	github.com/runsystemid/golog v1.0.11
 )
 
 require (
@@ -14,9 +15,12 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/fasthttp v1.52.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"log"
+	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/realclientip/realclientip-go"
+	"github.com/runsystemid/golog"
 )
 
 func getIPFromXForwardedFor() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		ips := c.Get("X-Forwarded-For")
-		log.Printf("print x-forwarded-for: %s\n", ips)
-		// log.Printf("context IP: %s\n", c.IPs())
-		return c.Next()
+		startTime := time.Now()
+		c.Locals("srcIP", c.Get("X-Forwarded-For"))
+
+		var err error
+		if _err := c.Next(); _err != nil {
+			err = _err
+		}
+
+		logMsg := golog.LogModel{
+			Header:       c.Request().Header.Header(),
+			Request:      c.Body(),
+			Method:       c.Method(),
+			HttpStatus:   uint64(c.Response().StatusCode()),
+			StatusCode:   strconv.Itoa(c.Response().StatusCode()),
+			Response:     string(c.Response().Body()),
+			ResponseTime: time.Since(startTime),
+			Error:        err,
+		}
+		golog.TDR(c.Context(), logMsg)
+
+		return err
 	}
 }
 
@@ -32,6 +52,19 @@ func realIPMiddleware() fiber.Handler {
 }
 
 func main() {
+	loggerConfig := golog.Config{
+		App:             "goreal",
+		AppVer:          "1.0.0",
+		Env:             "development",
+		FileLocation:    "logs/system.log",
+		FileTDRLocation: "logs/tdr.log",
+		FileMaxSize:     10,
+		FileMaxBackup:   10,
+		FileMaxAge:      10,
+		Stdout:          true,
+	}
+	golog.Load(loggerConfig)
+
 	app := fiber.New(fiber.Config{
 		// ProxyHeader:             fiber.HeaderXForwardedFor,
 		EnableTrustedProxyCheck: true,
